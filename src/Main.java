297961490bf1dade1419dbbc07b7c032b4b50c06
GaruCg <gary07caldera@gmail.com>
2024-04-03 17:18:00 -0600

    Commit de gary

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto de Analisis");
         System.out.println("Hola Mundo");
+        System.out.println(":c");
     }
 }
